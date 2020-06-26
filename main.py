7f5c7facee35d0742ef938a2e8ed27744e1d84bb
7enTropy7 <menon.uk1998@gmail.com>
2020-06-26 18:59:41 +0530

    main crap

--- main.py
+++ main.py
@@ -39,15 +39,14 @@ def form_blue_grid(grid):
         for i in range(len(t)):
             dec = t[i][0]*8+t[i][1]*4+t_next[i][0]*2+t_next[i][1]
             blue_grid.append(dec)
-    return blue_grid
+    blue = []
+    for i in range(0,len(blue_grid),4):
+        blue.append(blue_grid[i:i+4])
+    return blue
 
 def form_red_grid(grid):
     last_row = grid[len(grid)-1]
     # print(last_row)
-
-    transpose_grid = [[grid[j][i] for j in range(len(grid))] for i in range(len(grid[0]))] 
-    last_col = transpose_grid[len(transpose_grid)-1]
-    # print(last_col)
 
     red_grid = []
     for row in grid:
@@ -59,5 +58,46 @@ def form_red_grid(grid):
     red_grid = form_blue_grid(red_grid)
     return red_grid
 
-print(form_blue_grid(grid))
-print(form_red_grid(grid))
+blue_grid = form_blue_grid(grid)
+# print(blue_grid)
+
+
+red_grid = form_red_grid(grid)
+# print(red_grid)
+
+def convert_blue_decimalNeighbourhood_to_grid(blue_grid):
+    grid = []
+    for row in blue_grid:
+        odd_row = []
+        even_row = []
+        for i in row:
+            b = [int(j) for j in list('{0:0b}'.format(i))]
+            while(len(b)<4):
+                b.insert(0,0)
+            odd_row.append(b[0])
+            odd_row.append(b[1])
+            even_row.append(b[2])
+            even_row.append(b[3])
+        grid.append(odd_row)
+        grid.append(even_row)
+    return grid
+
+# print(convert_blue_decimalNeighbourhood_to_grid(red_grid))
+            
+
+def convert_red_decimalNeighbourhood_to_grid(red_grid):
+    '''
+    pop first row
+    insert first row at the end append
+    left shift by 1
+    '''
+    temp = convert_blue_decimalNeighbourhood_to_grid(red_grid)
+    t = temp[0]
+    temp.pop(0)
+    temp.append(t)
+    grid = []
+    for row in temp:
+        grid.append(right_shift(row,7))
+    return grid
+
+print(convert_red_decimalNeighbourhood_to_grid(red_grid))
