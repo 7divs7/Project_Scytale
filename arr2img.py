a95613ce6a3a870298def0ee678448bd4595a685
7enTropy7 <menon.uk1998@gmail.com>
2020-07-03 18:28:21 +0530

    list to image

--- arr2img.py
+++ arr2img.py
@@ -1,12 +1,16 @@
 import numpy as np
-import cv2
+import matplotlib.pyplot as plt 
 
-b = np.ones([300,300,1])
+grid = [[0, 1, 1, 1, 1, 0, 0, 1], 
+        [0, 1, 1, 1, 1, 1, 1, 1], 
+        [0, 1, 0, 0, 0, 0, 1, 0], 
+        [1, 1, 1, 0, 1, 1, 0, 0], 
+        [0, 0, 0, 1, 1, 1, 0, 0], 
+        [1, 0, 1, 1, 1, 1, 1, 1], 
+        [0, 0, 0, 0, 0, 1, 1, 0], 
+        [1, 0, 0, 0, 1, 0, 0, 1]]
 
-a = np.zeros([300,300,1])
-
-cv2.imshow('image1', a)
-cv2.imshow('image2', b)
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+x = np.asarray(grid)
+print(x.shape)
+plt.imshow(x, cmap='gray')
+plt.show()
