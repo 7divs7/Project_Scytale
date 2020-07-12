4f0d27d02bfdadfd14e93c59bab2fb22544633c8
Atharva <atharvahudlikar@gmail.com>
2020-07-12 19:28:51 +0530

    Code Updated

--- Image Augmentation/image_read.py
+++ Image Augmentation/image_read.py
@@ -3,8 +3,19 @@ import numpy as np
 
 img = cv2.imread('test.png')
 rows, cols, channels = img.shape
+bigger = cv2.resize(img, (256, 256)) 
+cv2.imshow('window',bigger)
 
-for i in range (0,rows):
-    for j in range (0,cols):
-        print(img[i][j][0],end = " ")
-    print('\n')
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+# import h5py
+# filename = ".h5"
+
+# with h5py.File(filename, "r") as f:
+#     # List all groups
+#     print("Keys: %s" % f.keys())
+#     a_group_key = list(f.keys())[0]
+
+#     # Get the data
+#     data = list(f[a_group_key])
